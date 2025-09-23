deb4d1769b2e6985b30fa20c529a0edaf1711ea3
Abraham <xdxdabrahamprololl10@gmail.com>
2025-09-23 13:00:11 +0800

    feat:(created base_class with the abstract class)

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+//import shits here
+//don't forget screenshots each and every progress.
+
+import 'attacker.dart';
+import 'plant.dart';
+
+void main{
+//put codes here for implementation of each class
+//erase the comments in this void after setting a zombie
+//then after you put codes here, put comments on
+//what the code do:>
+}
